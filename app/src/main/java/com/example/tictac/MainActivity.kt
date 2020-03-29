4e6ec5f5f5a8f9198a655c813f76deb9315e2d07
ayooubsmatti <ayooubsmatti@gmail.com>
2020-03-29 20:47:22 +0200

    Auto Player and Restart Game

--- app/src/main/java/com/example/tictac/MainActivity.kt
+++ app/src/main/java/com/example/tictac/MainActivity.kt
@@ -3,10 +3,12 @@ package com.example.tictac
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.Button
-import kotlin.math.log
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.random.Random
+import kotlin.random.Random.Default.nextInt
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,11 +17,14 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
-    @SuppressLint("ResourceAsColor")
-    fun buttonSelect(view:View){
-        var buchoice = view as Button
-        var cellId:Int = 0
-        when(buchoice.id){
+
+    fun buttonSelect( view:View){
+
+
+        val buSelected = view as Button
+
+        var cellId = 0
+        when(buSelected.id){
             R.id.button1 -> cellId = 1
             R.id.button2 -> cellId = 2
             R.id.button3 -> cellId = 3
@@ -30,28 +35,211 @@ class MainActivity : AppCompatActivity() {
             R.id.button8 -> cellId = 8
             R.id.button9 -> cellId = 9
         }
-        Log.d("cellId :",cellId.toString())
-        playeGame(buchoice,cellId)
+
+
+        //Log.d("buClick:", buSelected.id.toString())
+        // Log.d("buClick: cellId:",cellId.toString())
+
+        playGame(cellId,buSelected)
+
     }
+
+
+    var activePlayer = 1
 
     var player1 = ArrayList<Int>()
     var player2 = ArrayList<Int>()
-    var token:Int=1
 
-    fun playeGame(buchoice:Button,cellId:Int){
-       if (token == 1){
-           player1.add(cellId)
-           buchoice.text = "x"
-           buchoice.setBackgroundResource(R.color.colorAccent)
-           token = 2
-       }else{
+    fun playGame(cellId:Int, buSelected:Button){
+
+
+        if( activePlayer == 1 ){
+            buSelected.text = "X"
+            buSelected.setBackgroundResource(R.color.blue)
+            player1.add(cellId)
+            activePlayer = 2
+            autoPlay()
+
+        }else{
+
+            buSelected.text = "O"
+            buSelected.setBackgroundResource(R.color.darkGreen)
             player2.add(cellId)
-            buchoice.text = "o"
-            buchoice.setBackgroundResource(R.color.colorPrimary)
-            token = 1
+            activePlayer = 1
+
         }
 
-        buchoice.isEnabled = false
+        buSelected.isEnabled = false
+
+        checkWinner()
     }
+
+
+    fun checkWinner() {
+
+        var winer = -1
+
+
+        // row 1
+        if (player1.contains(1) && player1.contains(2) && player1.contains(3)) {
+            winer = 1
+        }
+        if (player2.contains(1) && player2.contains(2) && player2.contains(3)) {
+            winer = 2
+        }
+
+
+        // row 2
+        if (player1.contains(4) && player1.contains(5) && player1.contains(6)) {
+            winer = 1
+        }
+        if (player2.contains(4) && player2.contains(5) && player2.contains(6)) {
+            winer = 2
+        }
+
+        // row 3
+        if (player1.contains(7) && player1.contains(8) && player1.contains(9)) {
+            winer = 1
+        }
+        if (player2.contains(7) && player2.contains(8) && player2.contains(9)) {
+            winer = 2
+        }
+
+
+        // col 1
+        if (player1.contains(1) && player1.contains(4) && player1.contains(7)) {
+            winer = 1
+        }
+        if (player2.contains(1) && player2.contains(4) && player2.contains(7)) {
+            winer = 2
+        }
+
+
+        // col 2
+        if (player1.contains(2) && player1.contains(5) && player1.contains(8)) {
+            winer = 1
+        }
+        if (player2.contains(2) && player2.contains(5) && player2.contains(8)) {
+            winer = 2
+        }
+
+
+        // col 3
+        if (player1.contains(3) && player1.contains(6) && player1.contains(9)) {
+            winer = 1
+        }
+        if (player2.contains(3) && player2.contains(6) && player2.contains(9)) {
+            winer = 2
+        }
+        // cross
+        if (player1.contains(1) && player1.contains(5) && player1.contains(9)) {
+            winer = 1
+        }
+        if (player2.contains(1) && player2.contains(5) && player2.contains(9)) {
+            winer = 2
+        }
+
+        if (player1.contains(3) && player1.contains(5) && player1.contains(7)) {
+            winer = 1
+        }
+        if (player2.contains(3) && player2.contains(5) && player2.contains(7)) {
+            winer = 2
+        }
+
+
+        if (winer == 1) {
+            player1WinsCounts += 1
+            Toast.makeText(this, "Player 1 win the game", Toast.LENGTH_LONG).show()
+            restartGame()
+
+        } else if (winer == 2) {
+            player2WinsCounts += 1
+            Toast.makeText(this, "Player 2 win the game", Toast.LENGTH_LONG).show()
+            restartGame()
+        }
+
+
+    }
+
+    fun autoPlay(){
+
+
+        var emptyCells = ArrayList<Int>()
+
+        for( cellId in 1..9){
+
+            if( !(player1.contains(cellId) || player2.contains(cellId))){
+                emptyCells.add(cellId)
+            }
+        }
+
+
+
+        if(emptyCells.size==0){
+            restartGame()
+        }
+
+
+
+        val randIndex = nextInt(emptyCells.size)
+        val cellId = emptyCells[randIndex]
+
+        var buSelected:Button?
+        buSelected =  when(cellId){
+            1-> button1
+            2-> button2
+            3-> button3
+            4-> button4
+            5-> button5
+            6-> button6
+            7-> button7
+            8-> button8
+            9-> button9
+            else ->{ button1
+            }
+
+        }
+
+        playGame(cellId, buSelected)
+
+    }
+
+
+
+    var player1WinsCounts = 0
+    var player2WinsCounts = 0
+
+    fun restartGame(){
+
+        activePlayer = 1
+        player1.clear()
+        player2.clear()
+
+        for(cellId in 1..9){
+
+            val buSelected:Button? = when(cellId){
+                1-> button1
+                2-> button2
+                3-> button3
+                4-> button4
+                5-> button5
+                6-> button6
+                7-> button7
+                8-> button8
+                9-> button9
+                else ->{ button1}
+
+            }
+            buSelected!!.text = ""
+            buSelected.setBackgroundResource(R.color.whileBu)
+            buSelected.isEnabled = true
+        }
+
+        Toast.makeText(this,"Player1: $player1WinsCounts, Player2: $player2WinsCounts", Toast.LENGTH_LONG).show()
+
+
+    }
+
+
 
 }
